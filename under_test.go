380ae73e9dde05f487b4e8b9aa8d6f447c6df800
amnon <amnonbc@gmail.com>
2022-06-09 22:19:48 +0100

    moar tests

--- under_test.go
+++ under_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/cloudflare/cloudflare-go"
 	"io"
 	"net/http"
@@ -52,4 +53,65 @@ func Test_app_currentLevel(t *testing.T) {
 	got, err := a.currentLevel()
 	require.NoError(t, err)
 	assert.Equal(t, "medium", got)
+}
+
+type updateSettings struct {
+	Items []cloudflare.ZoneSetting `json:"items"`
+}
+
+func Test_app_setSecurityLevel(t *testing.T) {
+	patched := false
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			io.WriteString(w, settingsResponse)
+		case "PATCH":
+			patched = true
+			var us updateSettings
+			err := json.NewDecoder(r.Body).Decode(&us)
+			require.NoError(t, err)
+			require.Equal(t, 1, len(us.Items))
+			assert.Equal(t, "security_level", us.Items[0].ID)
+			assert.Equal(t, "under_attack", us.Items[0].Value)
+			io.WriteString(w, `{
+			  "success": true,
+			  "errors": [],
+			  "messages": []
+			}`)
+		}
+	}))
+	defer s.Close()
+
+	a := app{
+		zoneId: "myzone",
+	}
+	var err error
+	a.api, err = cloudflare.New("key", "email")
+	require.NoError(t, err)
+	a.api.BaseURL = s.URL
+	err = a.setSecurityLevel("under_attack")
+	assert.NoError(t, err)
+	assert.True(t, patched)
+}
+
+func Test_app_setSecurityLevelAlreadyThere(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			io.WriteString(w, settingsResponse)
+		default:
+			t.Error("There should not have been a patch request")
+		}
+	}))
+	defer s.Close()
+
+	a := app{
+		zoneId: "myzone",
+	}
+	var err error
+	a.api, err = cloudflare.New("key", "email")
+	require.NoError(t, err)
+	a.api.BaseURL = s.URL
+	err = a.setSecurityLevel("medium")
+	assert.NoError(t, err)
 }
